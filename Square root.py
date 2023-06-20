fe4b22355f9aa7dfbb2d76d115524674a8e5fe68
JPSE <45234686+sinawatra@users.noreply.github.com>
2023-06-21 00:20:17 +0700

    Add files via upload

---
+++ Square root.py
@@ -0,0 +1,7 @@
+number = int(input("Enter a number: "))
+square_root = number ** (1 / 2)
+print(square_root)
+
+number = int(input("Enter a number: "))
+square_root = number ** (1/2)
+print(square_root)
---
+++ upper and lower.py
@@ -0,0 +1,11 @@
+text="hello python"
+print(text.capitalize())
+text="hello python"
+print(text.upper())
+text="hello python"
+print(text.title())
+text="HELLO python"
+print(text.swapcase())
+text="HELLO PYTHON"
+print(text.lower())
+
